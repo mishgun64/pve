50d91f7ceacf16d6091c4e1b9fa1b3be7a3c7aa9
Mikhail Shubin <heimcbk201@gmail.com>
2026-03-11 21:27:48 +0300

    Fix terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,29 @@
 resource "proxmox_vm_qemu" "vm_media" {
-  name    = "media"
+  name        = "media"
+  agent       = 0
+  boot        = "order=scsi0;net0"
+  pxe         = true
   target_node = "pve"            # имя Proxmox node
-  vmid = 100              # уникальный VMID
-  cores   = 6
-  sockets = 1
-  memory = 2048              # MB
-  scsihw  = "virtio-scsi-pci"
-  boot    = "cdn"
+  vmid        = 100              # уникальный VMID
+  cores       = 6
+  sockets     = 1
+  memory      = 2048              # MB
+  scsihw      = "virtio-scsi-pci"
 
   # Диск
   disk {
-    size    = "5G"
-    type    = "scsi"
-    storage = "local-lvm"
+    size        = "5G"
+    type        = "disk"
+    disk_file   = "local:vm-<<<vmid>>>-disk-<<<disk number>>>"
+    storage     = "local"
   }
 
   # Сетевой интерфейс
   network {
-    model  = "virtio"
-    bridge = "vmbr0"
+    model     = "virtio"
+    bridge    = "vmbr0"
   }
 
   # ISO для установки ОС
-  iso = "local:iso/ubuntu-22.04-live-server-amd64.iso"
+  iso         = "local:iso/ubuntu-22.04-live-server-amd64.iso"
 }
